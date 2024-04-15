7160716ee1445f45faa67b88d7d88a6d9ada5bf1
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2024-04-15 20:48:24 +0200

    issue fixed with the escape char in google private key varible

--- app/google-sheets-api/getContent.ts
+++ app/google-sheets-api/getContent.ts
@@ -1,7 +1,5 @@
 import { GoogleApis, google } from 'googleapis'
 
-// const privateKey_one = process.env.private_key_part_one!.replace(/@/g, '\n');
-// const privateKey_two = process.env.private_key_part_two!.replace(/@/g, '\n');
 const privateKey = process.env.private_key!.replace(/@/g, '\n');
 
 // credentials for the google service account
