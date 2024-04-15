73bdb6a7bcc7915690f2253fa637c9bd75dc3257
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2024-04-15 17:42:34 +0200

    fix small bugs and tested all the api endpoints to google sheets in home page

--- app/google-sheets-api/getContent.ts
+++ app/google-sheets-api/getContent.ts
@@ -1,6 +1,26 @@
-import { google } from 'googleapis'
+import { GoogleApis, google } from 'googleapis'
 
-// getGeneralInformation
+// credentials for the google service account
+const serviceAccount = {
+    "type": "service_account",
+    "project_id": "fresh-generator-420014",
+    "private_key_id": process.env.private_key_id,
+    "private_key": process.env.private_key,
+    "client_email": process.env.client_email,
+    "client_id": process.env.client_id,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/forankrad-service-account%40fresh-generator-420014.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+  }
+  const auth = new google.auth.GoogleAuth({
+    credentials: serviceAccount,
+    scopes: ['https://www.googleapis.com/auth/spreadsheets']
+  })
+  const sheets = google.sheets({ version: 'v4', auth });
+
+// get general information for the website
 type GeneralInformation = {
     datum: string;
     om: string;
@@ -11,9 +31,7 @@ type GeneralInformation = {
     pris: string;
 };
 
-export async function getGeneralInformation(): Promise<GeneralInformation | undefined> {
-    const auth = await google.auth.getClient({ scopes: ['https://www.googleapis.com/auth/spreadsheets'] });
-    const sheets = google.sheets({ version: 'v4', auth });
+export async function getGeneralInformation(): Promise<GeneralInformation> {
     try {
         const res = await sheets.spreadsheets.values.get({
             spreadsheetId: process.env.GENERAL_INFO_ID,
@@ -31,16 +49,16 @@ export async function getGeneralInformation(): Promise<GeneralInformation | unde
                 bibelRef,
                 pris,
             }
+        } else {
+            throw new Error('No data found in the spreadsheet.');
         }
     } catch (error) {
-        console.error('Cannot fetch from google sheets:', error);
+        console.error('Cannot fetch from Google Sheets:', error);
+        throw error; // Re-throw the error to handle it in the calling code
     }
   }
-
 // getSpeakers return a 2d array with the structure of [namn, efternamn, titel, bildId]
 export async function getSpeakers(): Promise<string[][] | undefined> {
-    const auth = await google.auth.getClient({ scopes: ['https://www.googleapis.com/auth/spreadsheets'] });
-    const sheets = google.sheets({ version: 'v4', auth });
     try {
         const res = await sheets.spreadsheets.values.get({
             spreadsheetId: process.env.CONTRIBUTORS_ID,
@@ -53,11 +71,8 @@ export async function getSpeakers(): Promise<string[][] | undefined> {
         console.error('Cannot fetch from google sheets:', error);
     }
   }
-
 // getTestimonials return a 2d array with the structure [namn, tesimonial, bildId]
 export async function getTestimonials(): Promise<string[][] | undefined> {
-    const auth = await google.auth.getClient({ scopes: ['https://www.googleapis.com/auth/spreadsheets'] });
-    const sheets = google.sheets({ version: 'v4', auth });
     try {
         const res = await sheets.spreadsheets.values.get({
             spreadsheetId: process.env.CONTRIBUTORS_ID,
@@ -70,45 +85,23 @@ export async function getTestimonials(): Promise<string[][] | undefined> {
         console.error('Cannot fetch from google sheets:', error);
     }
   }
-
-// getSchedule returns a object with day and a 2d array with time and event
-// {
-//     'Fredag': [[tid, händelse], [tid, händelse]],
-//     'Lördag': [[tid, händelse], [tid, händelse]]
-// }
-type Schedule = {
-    [key: string]: Array<Array<string>>;
-  };
-
-export async function getSchedule(): Promise<Schedule | undefined> {
-    const auth = await google.auth.getClient({ scopes: ['https://www.googleapis.com/auth/spreadsheets'] });
-    const sheets = google.sheets({ version: 'v4', auth });
+// getSchedule returns a 2d array with time and event [[9:00, gudstjänst], [12:00, lunch]]
+export async function getSchedule(): Promise<string[][] | undefined> {
     try {
-        const res = await sheets.spreadsheets.get({
-            spreadsheetId: process.env.SCHEDULE_ID,
-        });
-        const allSheets: any = res.data.sheets
-        const scheduleObj: Schedule = {}
-        for(let i = 0; i < allSheets.length; i++) {
-            const sheetTitle: string = allSheets[i].properties?.title
             const res = await sheets.spreadsheets.values.get({
                 spreadsheetId: '1GuBDsm4rXyjMDv5iVf7PnPJZEkAdd4yDi7wzyjFIdiA',
-                range: sheetTitle
+                range: 'dag-1'
             });
             if (res.data.values) {
-                scheduleObj[sheetTitle] = res.data.values.splice(1);
-                return scheduleObj;
+                const dataWithoutFirstRow =  res.data.values.splice(1);
+                return dataWithoutFirstRow;
             }
-        }
     } catch (error) {
         console.error('Cannot fetch from google sheets:', error);
     }
   }
-
 // getFaQ returns a 2d array with question and answer [[question, answer], [question, answer]]
 export async function getFaQ(): Promise<string[][] | undefined> {
-    const auth = await google.auth.getClient({ scopes: ['https://www.googleapis.com/auth/spreadsheets'] });
-    const sheets = google.sheets({ version: 'v4', auth });
     try {
         const res = await sheets.spreadsheets.values.get({
             spreadsheetId: process.env.FAQ_ID,
--- app/page.tsx
+++ app/page.tsx
@@ -11,25 +11,65 @@ export default async function Home() {
   const testimonials = await getTestimonials();
   const faq = await getFaQ()
   const schedule = await getSchedule()
+  console.log(schedule)
   return (
     <div>
       <Navbar/>
     <div className='container flex justify-center mt-5'>
     <h1 className='font-bold text-xl text-lime-700'>Info från Google Kalkylark</h1>
     </div>
+    <div className='container flex justify-center mt-5 text-lime-800'>
+      <h1>General Info</h1>
+    </div>
     <div className='container flex justify-center mt-5'>
+
       <ul>
         <li>datum: {datum}</li>
         <li>Om: {om}</li>
         <li>tema: {tema}</li>
         <li>om temat: {omTema}</li>
         <li>bibelord: {bibelord}</li>
+        <li>bibelref: {bibelRef}</li>
+        <li>pris: {pris}</li>
       </ul>
     </div>
       <div className='container flex flex-wrap justify-center mt-5'>
       {speakers?.map((value, index) => (
           <div className='m-3' key={index}>
              <SpeakerCard namn={value[0]} efternamn={value[1]} titel={value[2]} bildId={value[3]}/>
+          </div>
+        ))}
+      </div>
+      <div className='container flex flex-wrap justify-center mt-5'>
+      {testimonials?.map((value, index) => (
+          <div className='m-3' key={index}>
+             <div>
+              <h1>{value[0]}</h1>
+              <p>{value[1]}</p>
+              <Image src={`https://drive.google.com/uc?id=${value[2]}`} alt={`bild på ${value[0]}`} width={100} height={100}/>
+             </div>
+          </div>
+        ))}
+      </div>
+      <div className='container flex justify-center mt-5 text-lime-800'>
+        <h1>Schema</h1>
+      </div>
+      <div className='flex justify-center'>
+        {schedule?.map((value, index) => (
+          <div key={index}>
+              <span>{value[0]}</span>
+              <span>{value[1]}</span>
+          </div>
+        ))}
+      </div>
+      <div className='container flex justify-center mt-5 text-lime-800'>
+        <h1>Frågor och svar</h1>
+      </div>
+      <div className='flex justify-center'>
+        {faq?.map((value, index) => (
+          <div key={index}>
+            <span> Question: {value[0]} </span>
+            <span> Answer: {value[1]} </span>
           </div>
         ))}
       </div>
