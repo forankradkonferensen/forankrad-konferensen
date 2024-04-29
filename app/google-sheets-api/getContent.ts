cb75b026a0434dc7163701c83f9f30614ec02ce3
Simon <simon.michael.te15d@gmail.com>
2024-04-29 22:16:27 +0200

    Changed SCHEDULE_ID, rest is prettier

--- app/google-sheets-api/getContent.ts
+++ app/google-sheets-api/getContent.ts
@@ -1,27 +1,28 @@
-import { GoogleApis, google } from 'googleapis'
+import { GoogleApis, google } from 'googleapis';
 
-// The private key is stored with placeholder @ and then replaced with the escape character \n bacause of deployment issuees
+// The private key is stored with placeholder @ and then replaced with the escape character \n because of deployment issues
 const privateKey = process.env.private_key!.replace(/@/g, '\n');
 
 // credentials for the google service account
 const serviceAccount = {
-    "type": "service_account",
-    "project_id": "fresh-generator-420014",
-    "private_key_id": process.env.private_key_id,
-    "private_key": privateKey,
-    "client_email": process.env.client_email,
-    "client_id": process.env.client_id,
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/forankrad-service-account%40fresh-generator-420014.iam.gserviceaccount.com",
-    "universe_domain": "googleapis.com"
-  }
-  const auth = new google.auth.GoogleAuth({
+    type: 'service_account',
+    project_id: 'fresh-generator-420014',
+    private_key_id: process.env.private_key_id,
+    private_key: privateKey,
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url:
+        'https://www.googleapis.com/robot/v1/metadata/x509/forankrad-service-account%40fresh-generator-420014.iam.gserviceaccount.com',
+    universe_domain: 'googleapis.com',
+};
+const auth = new google.auth.GoogleAuth({
     credentials: serviceAccount,
-    scopes: ['https://www.googleapis.com/auth/spreadsheets']
-  })
-  const sheets = google.sheets({ version: 'v4', auth });
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+});
+const sheets = google.sheets({ version: 'v4', auth });
 
 // get general information for the website
 type GeneralInformation = {
@@ -32,17 +33,18 @@ type GeneralInformation = {
     bibelord: string;
     bibelRef: string;
     pris: string;
+    plats: string;
 };
 
 export async function getGeneralInformation(): Promise<GeneralInformation> {
     try {
         const res = await sheets.spreadsheets.values.get({
             spreadsheetId: process.env.GENERAL_INFO_ID,
-            range: 'Sheet1'
+            range: 'Sheet1',
         });
-        const data: string[][] | undefined | null = res.data.values
-        if(data) {
-            const [datum, om, tema, omTema, bibelord, bibelRef, pris] = data[1]
+        const data: string[][] | undefined | null = res.data.values;
+        if (data) {
+            const [datum, om, tema, omTema, bibelord, bibelRef, pris, plats] = data[1];
             return {
                 datum,
                 om,
@@ -51,7 +53,8 @@ export async function getGeneralInformation(): Promise<GeneralInformation> {
                 bibelord,
                 bibelRef,
                 pris,
-            }
+                plats,
+            };
         } else {
             throw new Error('No data found in the spreadsheet.');
         }
@@ -59,61 +62,61 @@ export async function getGeneralInformation(): Promise<GeneralInformation> {
         console.error('Cannot fetch from Google Sheets:', error);
         throw error; // Re-throw the error to handle it in the calling code
     }
-  }
+}
 // getSpeakers return a 2d array with the structure of [namn, efternamn, titel, bildId]
 export async function getSpeakers(): Promise<string[][] | undefined> {
     try {
         const res = await sheets.spreadsheets.values.get({
             spreadsheetId: process.env.CONTRIBUTORS_ID,
-            range: 'talare'
+            range: 'talare',
         });
-        const data: string[][] | undefined | null = res.data.values
-        const dataWithoutFirstRow = data?.splice(1)
+        const data: string[][] | undefined | null = res.data.values;
+        const dataWithoutFirstRow = data?.splice(1);
         return dataWithoutFirstRow;
     } catch (error) {
         console.error('Cannot fetch from google sheets:', error);
     }
-  }
-// getTestimonials return a 2d array with the structure [namn, tesimonial, bildId]
+}
+// getTestimonials return a 2d array with the structure [namn, testimonial, bildId]
 export async function getTestimonials(): Promise<string[][] | undefined> {
     try {
         const res = await sheets.spreadsheets.values.get({
             spreadsheetId: process.env.CONTRIBUTORS_ID,
-            range: 'testimonials'
+            range: 'testimonials',
         });
-        const data: string[][] | undefined | null = res.data.values
-        const dataWithoutFirstRow: string[][] | undefined | null  = data?.splice(1)
+        const data: string[][] | undefined | null = res.data.values;
+        const dataWithoutFirstRow: string[][] | undefined | null = data?.splice(1);
         return dataWithoutFirstRow;
     } catch (error) {
         console.error('Cannot fetch from google sheets:', error);
     }
-  }
+}
 // getSchedule returns a 2d array with time and event [[9:00, gudstjänst], [12:00, lunch]]
 export async function getSchedule(): Promise<string[][] | undefined> {
     try {
-            const res = await sheets.spreadsheets.values.get({
-                spreadsheetId: '1GuBDsm4rXyjMDv5iVf7PnPJZEkAdd4yDi7wzyjFIdiA',
-                range: 'dag-1'
-            });
-            if (res.data.values) {
-                const dataWithoutFirstRow =  res.data.values.splice(1);
-                return dataWithoutFirstRow;
-            }
+        const res = await sheets.spreadsheets.values.get({
+            spreadsheetId: process.env.SCHEDULE_ID,
+            range: 'Sheet1',
+        });
+        if (res.data.values) {
+            const dataWithoutFirstRow = res.data.values.splice(1);
+            return dataWithoutFirstRow;
+        }
     } catch (error) {
         console.error('Cannot fetch from google sheets:', error);
     }
-  }
+}
 // getFaQ returns a 2d array with question and answer [[question, answer], [question, answer]]
 export async function getFaQ(): Promise<string[][] | undefined> {
     try {
         const res = await sheets.spreadsheets.values.get({
             spreadsheetId: process.env.FAQ_ID,
-            range: 'Sheet1'
+            range: 'Sheet1',
         });
-        const data: string[][] | undefined | null = res.data.values
-        const dataWithoutFirstRow: string[][] | undefined | null = data?.splice(1)
+        const data: string[][] | undefined | null = res.data.values;
+        const dataWithoutFirstRow: string[][] | undefined | null = data?.splice(1);
         return dataWithoutFirstRow;
     } catch (error) {
         console.error('Cannot fetch from google sheets:', error);
     }
-  }
+}
