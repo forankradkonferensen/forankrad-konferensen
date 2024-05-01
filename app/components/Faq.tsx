721d04033a7cbb339df4a41a05e48bdccb23ab6b
Simon <simon.michael.te15d@gmail.com>
2024-05-01 17:25:25 +0200

    Styling changes - NEEDS ADJUSTMENTS WHEN ACTIVE

--- app/components/Faq.tsx
+++ app/components/Faq.tsx
@@ -2,21 +2,21 @@
 import { useState } from "react"
 
 type FaqTypes = {
-    question: string;
-    answer: string;
+  question: string;
+  answer: string;
 }
 
-const Faq: React.FC<FaqTypes> = ({question, answer}) => {
-    const [showContent, setShowContent] = useState(false)
+const Faq: React.FC<FaqTypes> = ({ question, answer }) => {
+  const [showContent, setShowContent] = useState(false)
   return (
     <div>
-        <div className="flex flex-row">
-            <button className="btn basis-1/4" onClick={() => setShowContent(prev => !prev) }>{showContent ? '-' : '+' }</button>
-            <div>
-            <p className="basis-3/4">{question}</p>
-            <p className="basis-3/4">{showContent && answer}</p>
-            </div>
+      <div className="flex flex-row items-center">
+        <button className="btn btn-circle bg-white" onClick={() => setShowContent(prev => !prev)}>{showContent ? '-' : '+'}</button>
+        <div>
+          <p className="basis-3/4 p-2">{question}</p>
+          <p className="basis-3/4">{showContent && answer}</p>
         </div>
+      </div>
     </div>
   )
 }
