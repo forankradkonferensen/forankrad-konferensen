5fe5751adb3481b330615000660f29b08b535891
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2024-05-02 19:51:51 +0200

    fix navbar design to match figma mockup

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,21 +5,17 @@ const Navbar = () => {
         <div className="navbar bg-black">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                    <div>
+                        <p>Instagram</p>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-black rounded-box w-52">
-                        <li><a>Hem</a></li>
-                        <li><a>Boka/avboka</a></li>
-                        <li><a>Kontakt</a></li>
-                    </ul>
                 </div>
             </div>
             <div className="navbar-center">
                 <a className="btn btn-ghost text-xl">Förankrad Konferensen</a>
             </div>
             <div className="navbar-end">
-                Social media
+                <p className='mr-5'><a>Hem</a></p>
+                <p className='mr-5'><a>Anmälan</a></p>
             </div>
         </div>
     )
