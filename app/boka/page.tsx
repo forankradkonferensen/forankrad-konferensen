1c20548916bfb7a483363b840c89797ed76dc454
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2024-05-05 20:47:04 +0200

    remove validator

--- app/boka/page.tsx
+++ app/boka/page.tsx
@@ -4,12 +4,12 @@ import { bookEvent } from "../google-sheets-api/eventBooking";
 const handleBooking = async (name: string, lastname: string, email: string, ) => {
     'use server'
     try {
-        await bookEvent([name, lastname, email, 'nej']);
+            await bookEvent([name, lastname, email, 'nej']);
+
     } catch (error) {
         console.error('Failed to book event:', error);
     }
 }
-
 const Boka = () => {
     return (
         <div>
