9e1750fd03069719bc606f5e9a60838b0c0e4546
Simon <simon.michael.te15d@gmail.com>
2024-05-15 11:57:52 +0200

    Typos and formatting

--- app/actions.ts
+++ app/actions.ts
@@ -1,57 +1,72 @@
-'use server'
-import { bookEvent } from "./google-sheets-api/eventBooking";
-import { EmailTemplate } from "./components/EmailTemplate";
+'use server';
+import { bookEvent } from './google-sheets-api/eventBooking';
+import { EmailTemplate } from './components/EmailTemplate';
 import { Resend } from 'resend';
 
 export const handleBooking = async (prevState: any, formData: FormData) => {
-    const name = formData.get("name")?.toString() ?? "";
-    const lastname = formData.get("lastName")?.toString() ?? "";
-    const email = formData.get("email")?.toString() ?? "";
-    const confirmEmail = formData.get("emailConfirmation")?.toString() ?? "";
-    const personalNumber = formData.get("personalNumber")?.toString().trim() ?? "";
-    const phoneNumber = formData.get("phoneNumber")?.toString().trim() ?? "";
-    const denomination = formData.get("denomination")?.toString() ?? "";
-    const message = formData.get("message")?.toString() ?? "";
-    const approve = formData.get("approve")
-    const hasPaied = 'nej'
+    const name = formData.get('name')?.toString() ?? '';
+    const lastName = formData.get('lastName')?.toString() ?? '';
+    const email = formData.get('email')?.toString() ?? '';
+    const confirmEmail = formData.get('emailConfirmation')?.toString() ?? '';
+    const personalNumber = formData.get('personalNumber')?.toString().trim() ?? '';
+    const phoneNumber = formData.get('phoneNumber')?.toString().trim() ?? '';
+    const denomination = formData.get('denomination')?.toString() ?? '';
+    const message = formData.get('message')?.toString() ?? '';
+    const approve = formData.get('approve');
+    const hasPaid = 'nej';
 
     if (email !== confirmEmail) {
         return { message: 'Email fälten matchar inte' };
     }
-    if(approve !== 'on') {
+    if (approve !== 'on') {
         return { message: 'Du måste godkänna att vi behandlar dina personuppgifter' };
-        }
-    if(personalNumber.length !== 12) {
-        return { message: 'Personnummret borde vara i detta format: 199507120000' };
     }
-    if(phoneNumber.length !== 12 || phoneNumber[0] !== '+') {
-       return {message: 'telefonnummret måste vara i detta formatet +46xxxxxxxxx'}
+    if (personalNumber.length !== 12) {
+        return { message: 'Personnumret borde vara i detta format: 199507120000' };
+    }
+    if (phoneNumber.length !== 12 || phoneNumber[0] !== '+') {
+        return { message: 'telefonnumret måste vara i detta formatet +46xxxxxxxxx' };
     }
     try {
-        const book = await bookEvent([name, lastname, personalNumber, email, phoneNumber, denomination, message, hasPaied]);
-        if(book instanceof Error) {
-         if(book.message === 'fullbokat') {
+        const book = await bookEvent([
+            name,
+            lastName,
+            personalNumber,
+            email,
+            phoneNumber,
+            denomination,
+            message,
+            hasPaid,
+        ]);
+        if (book instanceof Error) {
+            if (book.message === 'fullbokat') {
                 return { message: 'Ledsen det finns inga platser lediga just nu' };
             }
-         if(book.message === 'server') {
-                return {message: 'Det gick inte att boka just nu, testa igen om en liten stund'}
+            if (book.message === 'server') {
+                return { message: 'Det gick inte att boka just nu, testa igen om en liten stund' };
             }
         }
-        const sendEmail = await sendEmailBookingConfirmation(email, name); 
-        if(sendEmail instanceof Error) {
-            return { message: 'Vi kunde inte skicka ett email till dig, Försök igen eller kontakta oss på forankradkonferensen@gmail.com' };        
+        const sendEmail = await sendEmailBookingConfirmation(email, name);
+        if (sendEmail instanceof Error) {
+            return {
+                message:
+                    'Vi kunde inte skicka ett email till dig, Försök igen eller kontakta oss på forankradkonferensen@gmail.com',
+            };
         }
-        if(book instanceof Error) {
-            if(book.message === 'dubbelbokning') {
-                return { message: 'Du har redan påbörjat en bokning! Vi skickar ett nytt mail till dig så du kan sluföra bokningen..' };
+        if (book instanceof Error) {
+            if (book.message === 'dubbelbokning') {
+                return {
+                    message:
+                        'Du har redan påbörjat en bokning! Vi skickar ett nytt mail till dig så du kan slutföra bokningen..',
+                };
             }
-        } 
-        return {message: 'Tack, kolla din email för att slutföra bokningen!'}
+        }
+        return { message: 'Tack, kolla din email för att slutföra bokningen!' };
     } catch (error) {
         console.error('Failed to book event:', error);
-            return { message: 'Det gick inte att boka eventet, testa igen om en liten stund' };
+        return { message: 'Det gick inte att boka eventet, testa igen om en liten stund' };
     }
-}
+};
 
 export const sendEmailBookingConfirmation = async (email: string, name: string) => {
     const resend = new Resend(process.env.RESENDKEY);
@@ -61,12 +76,12 @@ export const sendEmailBookingConfirmation = async (email: string, name: string) 
             to: [email],
             subject: 'Slutför bokning',
             react: EmailTemplate({ firstName: name }) as React.ReactElement,
-        })
-        if(data.error) {
-            return new Error()
+        });
+        if (data.error) {
+            return new Error();
         }
     } catch (err) {
-        console.log(err)
+        console.log(err);
         return new Error();
     }
-}
+};
