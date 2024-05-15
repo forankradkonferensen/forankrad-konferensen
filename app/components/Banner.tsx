a2035d7f1648a416280c83c92ee07c64d1fb338f
Simon <simon.michael.te15d@gmail.com>
2024-05-15 13:43:37 +0200

    Fixed styling and logic

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -17,10 +17,12 @@ const Banner: React.FC<BannerTypes> = ({ title, text, bibleRef, buttonText, butt
             </video>
 
             <div className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 z-10 flex flex-col justify-center items-center'>
-                <h1 className='text-3xl text-center'>{title}</h1>
-                <h4 className='text-xl text-center p-2'>{text}</h4>
+                <h1 className='text-5xl text-center font-semibold tracking-wider'>{title}</h1>
+                <h4 className='text-2xl text-center p-2'>{text}</h4>
                 <p className='text-center p-2'>{bibleRef}</p>
-                <a className='btn bg-white hover:bg-whiteShade outline-none w-32 text-black' href={buttonHref}>{buttonText}</a>
+                {buttonHref && buttonText && (
+                    <a className='btn bg-white hover:bg-whiteShade outline-none w-32 text-black' href={buttonHref}>{buttonText}</a>
+                )}
             </div>
 
             <img className='absolute bottom-0 left-0 w-3/5 pl-1' src={image} alt="" />
