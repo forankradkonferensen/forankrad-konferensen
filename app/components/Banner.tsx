22d8ff11a71ec09766bf9f3a6fa8defebedf057a
Emanuel Gustafzon <104900900+EmanuelGustafzon@users.noreply.github.com>
2024-05-16 19:38:37 +0200

    Update Banner.tsx
    Test css banner

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -11,22 +11,23 @@ type BannerTypes = {
 
 const Banner: React.FC<BannerTypes> = ({ title, text, bibleRef, buttonText, buttonHref, image }) => {
     return (
-        <div className='relative h-full'>
-            <video width="100%" height="auto" autoPlay muted loop playsInline className='opacity-40 disable-opacity w-full'>
-                <source src='backgroundVideo.mp4' type="video/mp4" />
-            </video>
+       <div className="relative h-screen overflow-hidden">
+    <video width="100%" height="auto" autoPlay muted loop playsInline className="opacity-40 disable-opacity w-full">
+        <source src="backgroundVideo.mp4" type="video/mp4" />
+    </video>
 
-            <div className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 z-10 flex flex-col justify-center items-center'>
-                <h1 className='text-5xl text-center font-semibold tracking-wider'>{title}</h1>
-                <h4 className='text-2xl text-center p-2 font-medium'>{text}</h4>
-                <p className='text-center'>{bibleRef}</p>
-                {buttonHref && buttonText && (
-                    <a className='btn bg-white hover:bg-whiteShade outline-none w-32 text-black' href={buttonHref}>{buttonText}</a>
-                )}
-            </div>
+    <div className="absolute inset-0 flex flex-col justify-center items-center text-center z-10">
+        <h1 className="text-4xl md:text-5xl font-semibold tracking-wider">{title}</h1>
+        <h4 className="text-lg md:text-2xl p-2 font-medium">{text}</h4>
+        <p className="text-sm md:text-base">{bibleRef}</p>
+        {buttonHref && buttonText && (
+            <a className="btn bg-white hover:bg-whiteShade outline-none w-32 md:w-48 text-black" href={buttonHref}>{buttonText}</a>
+        )}
+    </div>
 
-            <img className='absolute bottom-0 left-0 w-3/4 pl-1' src={image} alt="" />
-        </div>
+    <img className="absolute bottom-0 left-0 w-3/4 pl-1" src={image} alt="" />
+</div>
+
     );
 }
 
